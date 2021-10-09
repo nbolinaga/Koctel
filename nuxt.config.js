eae31ad76705d7a60eb2a3cadbb5772e4cb092b2
nbolinaga <nbolinaga17@gmail.com>
2021-10-08 20:17:14 -0400

    fixed colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,14 +64,23 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.red.darken2,
+          accent: colors.grey.lighten5,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        light: {
+          primary: colors.red.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
+        },
       }
     }
   },
