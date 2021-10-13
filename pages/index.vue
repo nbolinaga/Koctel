d13bed33730500fc5ed6e09e3978634cfd78b278
nbolinaga <nbolinaga17@gmail.com>
2021-10-13 11:04:41 -0400

    sanity setup

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-md-12">
         <h1>Koctel Main Page</h1>
+        <h3 class='primario--text'>{{cocteles != null ? 'Conectado': 'No Conectado'}}</h3>
         <p>{{cocteles}}</p>
       </div>
     </div>
@@ -11,12 +12,12 @@
 
 <script>
 import { groq } from "@nuxtjs/sanity";
-import sanity from "@sanity/client"
-
+// import sanity from "@sanity/client"
 export default {
+  
   async asyncData({ $sanity }) {
     const query = groq`*[_type == "coctel"]`;
-    const cocteles = await $sanity.fetch(query);
+    const cocteles = await $sanity.fetch(query).catch(() => console.log("Error"));
     return { cocteles };
   }
 };
