81d8f2f8fd9baaff2a88d1720e85c9f1eebd5bbd
nbolinaga <nbolinaga17@gmail.com>
2021-10-13 15:24:24 -0400

    agregados fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,3 +15,13 @@
   }
 };
 </script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600&family=Raleway:ital,wght@0,400;0,500;1,300&display=swap');
+@import '../assets/variables.scss';
+
+.titulo{
+  font-family: $titulos;
+  font-weight: 600;
+}
+</style>
