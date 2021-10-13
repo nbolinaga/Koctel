339f91d140a5ef775af6515261b62b99ec8625ea
nbolinaga <nbolinaga17@gmail.com>
2021-10-13 19:27:58 -0400

    eliminado prueba y empezada landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Koctel Main Page</h1>
-        <h3 class='primario--text'>{{cocteles != null ? 'Conectado': 'No Conectado'}}</h3>
-        <p>{{cocteles}}</p>
-      </div>
-    </div>
+    <LogoCompleto class="logoCompleto mt-10" fill="#9C3B3B"/>
   </div>
 </template>
 
-<script>
-import { groq } from "@nuxtjs/sanity";
-// import sanity from "@sanity/client"
-export default {
-  
-  async asyncData({ $sanity }) {
-    const query = groq`*[_type == "coctel"]`;
-    const cocteles = await $sanity.fetch(query).catch(() => console.log("Error"));
-    return { cocteles };
+<style lang="scss">
+  .container {
+    min-height: 30vh;
+    margin: 0 auto;
   }
-};
-</script>
+
+  .logoCompleto{
+    widows: auto;
+    height: 40vh;
+  }
+</style>
