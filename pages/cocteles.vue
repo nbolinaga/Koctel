37dc2a88f2efe540b43364cbde309d8c8ec65fea
nbolinaga <nbolinaga17@gmail.com>
2021-10-15 09:13:28 -0400

    pagina cocteles

---
+++ pages/cocteles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <ListaCocteles/>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+  .container {
+    min-height: 30vh;
+    margin: 0 auto;
+  }
+</style>
