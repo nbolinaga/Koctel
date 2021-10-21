697f020f9e3cbca63c67c9966aebb0a92fbdf81c
nbolinaga <nbolinaga17@gmail.com>
2021-10-21 00:02:11 -0400

    componentes anadidos

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <LogoCompleto class="logoCompleto mt-10" fill="#9C3B3B"/>
-    <ListaCocteles />
+    <h2 id="subtitulo" class="titulo primario--text d-flex justify-center">TU BARTENDER DE BOLSILLO</h2>
+    <v-divider class="detalles my-12"></v-divider>
+    <Cocteles id="cocteles"/>
+    <v-divider class="detalles my-12"></v-divider>
+    <Bartender id="bartender"/>
   </div>
 </template>
 <script>
@@ -16,5 +20,13 @@
   .logoCompleto{
     widows: auto;
     height: 40vh;
+  }
+  #cocteles, #bartender{
+    padding-top: 70px;
+    margin-top: -70px;
+  }
+  #subtitulo{
+    margin-top: -100px;
+    padding-bottom: 100px;
   }
 </style>
