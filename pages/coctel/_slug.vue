4e4403e5ba75dd55937e1a2b28c053408ea2b32f
nbolinaga <nbolinaga17@gmail.com>
2021-11-02 01:52:26 -0400

    agregado rating

--- pages/coctel/_slug.vue
+++ pages/coctel/_slug.vue
@@ -7,8 +7,9 @@
                     <v-img :src="coctel.img" alt="" height="100%" width="auto" cover/>
                 </v-col>
                 <v-col cols="12" md="8" class="texto pt-10">
+                    <!-- <v-rating v-model="rating" half-increments background-color="secundario lighten-3" color="secundario" small class="d-flex justify-center"></v-rating> -->
                     <h2 class="primario--text titulo d-flex justify-center">{{coctel.nombre}}</h2>
-                    <v-list class="texto mx-0 px-0 my-2 mb-10">
+                    <v-list class="texto mx-0 px-0 my-2">
                         <v-list-item v-for="(alcohol, index) in coctel.alcohol" :key="alcohol.id" class="my-n5 mx-12 no-background-hover" nuxt :to="`/alcohol/${alcohol.id.current}`" :ripple="false">
                              <v-list-item-title class="alt--text d-flex justify-center textosDetalles" ><div v-if="alcohol.nombre != 'Sin Alcohol'" class="mr-1">{{coctel.cantidades[index]}} - </div>{{alcohol.nombre}}</v-list-item-title>
                         </v-list-item>
@@ -19,6 +20,10 @@
                         <v-list-item class="mt-10 no-background-hover" nuxt :to="`/pais/${coctel.pais.id.current}`" :ripple="false">
                             <v-list-item-title  id="pais" class="primario--text d-flex justify-center titulo">{{coctel.pais.nombre}}</v-list-item-title>
                         </v-list-item>
+                        <v-list-item class="d-flex justify-center mt-10 mb-5">
+                            <v-rating v-model="rating" half-increments background-color="alt lighten-3" color="alt" large></v-rating>
+                        </v-list-item>
+                        
                     </v-list>
                 </v-col>
             </v-row>
@@ -36,7 +41,8 @@ export default {
     data() {
         return {
             coctel: {},
-            loading: true
+            loading: true,
+            rating: 5,
         }
     },
     beforeMount() {
