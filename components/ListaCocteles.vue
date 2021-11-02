6ff102fe2adad572079e46af8376df5ac9a42cae
nbolinaga <nbolinaga17@gmail.com>
2021-11-02 01:52:10 -0400

    incrementado tamano de imagen

--- components/ListaCocteles.vue
+++ components/ListaCocteles.vue
@@ -6,7 +6,7 @@
           <v-card v-for="coctel in coctelesFiltrados" id="ingredientes" :key="coctel._id" class="my-5 texto overflow-hidden" width="25vw" elevation="5" tile :to="`/coctel/${coctel.slug.current}`">
             <v-card-title class="textos primario--text align-end titulo">{{coctel.nombre}}<v-icon v-if="coctel.categoria.includes('Sin Alcohol')" class="pb-1 alt--text" right>{{sinAlcoholLogo}}</v-icon><v-icon  v-if="coctel.categoria.includes('Light')" class="pb-1 secundario--text" right>{{sinAzucar}}</v-icon></v-card-title>
             <v-card-subtitle class="d-flex inline textos alt--text"><p v-for="alcohol in coctel.nombreAlcohol" :key="alcohol.nombre">{{alcohol}}</p></v-card-subtitle>
-            <v-img :src="coctel.img" width="100%" height="10vh" cover></v-img>
+            <v-img :src="coctel.img" width="100%" height="20vh" cover right></v-img>
           </v-card>
         </v-row>
         <v-row v-if="incompletos" class="d-flex justify-space-around">
