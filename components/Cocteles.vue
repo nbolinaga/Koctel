e4c0f322c44bb2cbffb8a2c4d900e131351bda9a
nbolinaga <nbolinaga17@gmail.com>
2021-11-09 01:34:16 -0400

    evita correr varios filtros a la vez

--- components/Cocteles.vue
+++ components/Cocteles.vue
@@ -3,15 +3,15 @@
     <h2 class="titulo primario--text mb-8">Nuestros cocteles</h2>
     <p class="textos texto--text">A continuación encontrarás nuestra gran gama de cocteles de todas partes del mundo, donde te ofrecemos tragos desde los más populares hasta cocteles sin alcohol o sin azúcar. En Koctel siempre estamos actualizando nuestro catálogo de cocteles con los más nuevos</p>
     <div class="d-flex">
-      <v-switch v-model="lightFiltro" dark :label="`Solo Light`"></v-switch>
+      <v-switch v-model="lightFiltro" dark :label="`Solo Light`" :disabled="pais != null || nombre != null"></v-switch>
       <v-icon class="pb-2 primario--text" right>{{sinAzucar}}</v-icon>
     </div>
     <div class="d-flex">
-      <v-switch v-model="sinAlcohol" dark :label="`Sin Alcohol`"></v-switch>
+      <v-switch v-model="sinAlcohol" dark :label="`Sin Alcohol`" :disabled="pais != null || nombre != null"></v-switch>
       <v-icon class="pb-2 detalles--text" right>{{sinAlcoholLogo}}</v-icon>
     </div>
-    <v-autocomplete  v-if="!loading" v-model="nombre" dark :label="`Buscar por Nombre`" :items="cocteles" item-text="nombre" clearable></v-autocomplete>
-    <v-autocomplete  v-if="!loading" v-model="pais" dark :label="`Buscar por Pais`" :items="paises" item-text="nombre" clearable></v-autocomplete>
+    <v-autocomplete  v-if="!loading" v-model="nombre" dark :label="sinAlcohol || lightFiltro ? `Desactivar filtros antes de usar` : `Buscar por Nombre`" :items="cocteles" item-text="nombre" clearable :disabled="lightFiltro == true || sinAlcohol == true"></v-autocomplete>
+    <v-autocomplete  v-if="!loading" v-model="pais" dark :label="sinAlcohol || lightFiltro ? `Desactivar filtros antes de usar`: `Buscar por Pais`" :items="paises" item-text="nombre" clearable :disabled="lightFiltro == true || sinAlcohol == true"></v-autocomplete>
     <ListaCocteles :key="key" :light-filtro="lightFiltro" :sin-alcohol-filtro="sinAlcohol" :nombre="nombre" :pais="pais"/>
   </div>
 </template>
